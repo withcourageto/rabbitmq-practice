927e7b75b2ba456e7689da5d57e78edd1d17ba43
cool-moon <cuitlideng@163.com>
2017-11-02 16:44:20 +0800

    target

--- hello-rabbitmq/src/main/java/top/cmoon/rabbitmq/learn/Send.java
+++ hello-rabbitmq/src/main/java/top/cmoon/rabbitmq/learn/Send.java
@@ -2,58 +2,27 @@ package top.cmoon.rabbitmq.learn;
 
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import top.cmoon.rabbitmq.learn.util.ArgUtil;
+import top.cmoon.rabbitmq.learn.util.ChannelUtil;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
-import java.util.stream.Stream;
 
 public class Send {
 
     private static final String QUEUE_NAME = "hello";
-    private static final String DELIMITER = " ";
 
     public static void main(String[] args) throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        Channel channel = ChannelUtil.getChannel();
 
         channel.exchangeDeclare("logs", BuiltinExchangeType.FANOUT);
 
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = getMessage(args);
+        channel.queueDeclare(QUEUE_NAME, false, false, true, null);
+        String message = ArgUtil.getMessage(args);
         channel.basicPublish("", QUEUE_NAME, null
                 , message.getBytes());
 
-
-
-
         System.out.println("[X] Sent '" + message + "'");
-        channel.close();
-        connection.close();
+        ChannelUtil.closeChannelAndConn(channel);
     }
-
-    private static String getMessage(String[] strings) {
-        if (strings.length < 1) {
-            return "hello world";
-        }
-        return joinStrings(strings, DELIMITER);
-    }
-
-    private static String joinStrings(String[] strings, String delimiter) {
-        if (strings.length == 0) {
-            return "";
-        }
-        StringBuilder result = new StringBuilder(strings[0]);
-
-        Stream.of(strings)
-                .skip(1)
-                .forEach(s -> result.append(delimiter).append(s));
-
-        return result.toString();
-    }
-
 }
--- topic/src/main/java/top/cmoon/rabbitmq/learn/topic/EmitLogTopic.java
+++ topic/src/main/java/top/cmoon/rabbitmq/learn/topic/EmitLogTopic.java
@@ -1,4 +1,30 @@
 package top.cmoon.rabbitmq.learn.topic;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import top.cmoon.rabbitmq.learn.util.ArgUtil;
+import top.cmoon.rabbitmq.learn.util.ChannelUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class EmitLogTopic {
+
+    private static final String exchange_name = "topic_logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        Channel channel = ChannelUtil.getChannel();
+
+        channel.exchangeDeclare(exchange_name, BuiltinExchangeType.TOPIC);
+
+        String routingKey = ArgUtil.getRouting(args).orElse("info");
+        String message = ArgUtil.getMessage(args, 1);
+
+        channel.basicPublish(exchange_name, routingKey, null
+                , message.getBytes());
+
+        System.out.println("[X] Sent '" + routingKey + "':'" + message + "'");
+        ChannelUtil.closeChannelAndConn(channel);
+    }
 }
